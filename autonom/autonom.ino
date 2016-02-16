fbbf8c260f843895a5e26dea32821fc3a617c84e
martinlunde <martlun@stud.ntnu.no>
2016-02-16 15:31:49 +0100

    ryddet i koden

--- autonom/autonom.ino
+++ autonom/autonom.ino
@@ -3,22 +3,15 @@
 
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
-
-// This is the maximum speed the motors will be allowed to turn.
-// (400 lets the motors go at top speed; decrease to impose a speed limit)
 const int MAX_SPEED = 400;
 
-void setup()
-{
-
- // Wait for the user button to be pressed and released
-
+void setup() {
  button.waitForButton();
 
- motors.setSpeeds(100,100); //set left and right motor speed
-  delay(1000);
-  motors.setSpeeds(0,0);
-
+ motors.setSpeeds(100,100);
+ delay(1000);
+ motors.setSpeeds(0,0);
 } 
+
 void loop() { 
 }
